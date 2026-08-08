bf18aea0e7b8635b67ee70b34bb4d5ba58df35ea
VSCode User <vscode@local.dev>
2026-08-08 12:48:49 +0900

    0808 03

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 async def index():
     return {"message": "Hello, World!"}
 
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
